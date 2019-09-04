7e9a6f55997f256a1d3b6c311e6a9d2c9c462e70
jaeryo2357 <jaeryo2357@naver.com>
2019-09-04 18:59:17 +0900

    로그인 수정

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,12 +13,8 @@ class UserController extends Controller
     {
         $phone=request('id','');
         $pwd=request('pw','');
-        if (Auth::attempt(['ID' => $phone, 'PW' => $pwd])) {
-
-
-            return C::RESULT_ARRAY_SUCCESS("성공");
-            // Authentication passed...
-//            return redirect()->intended('dashboard');
+        if (User::where('ID',$phone)->where('PW',$pwd)->exists()){
+            return C::RESULT_ARRAY_SUCCESS("성공.");
         } else {
             return C::RESULT_ARRAY_ERROR("아이디와 비밀번호가 일치하지 않습니다.");
         }
